22c2b8b4d76457f18b7665b50805e016d5c0c2e5
Mohammed <ibytemaverick@gmail.com>
2025-04-14 23:18:25 -0700

    Almost done with UI for the login page.

---
+++ View/forgot_password_view.py
@@ -0,0 +1,193 @@
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QLabel, QLineEdit,
+    QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QFormLayout, QFrame
+)
+from PyQt5.QtGui import QFont, QCursor
+from PyQt5.QtCore import Qt
+import sys
+
+
+
+class ForgotPasswordWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PennyPilot")
+        self.setGeometry(700, 500, 900, 700)
+        self.setStyleSheet("background-color: white;")
+
+
+        main_layout = QVBoxLayout()
+
+        # Title
+        title = QLabel("PennyPilot")
+
+        title.setAlignment(Qt.AlignCenter)
+        title.setStyleSheet(
+            """
+            color: #000;
+            text-align: center;
+            font-family: Inter;
+            font-size: 32px;
+            font-style: normal; 
+            font-weight: 600;
+            line-height: 150%; /* 48px */
+            letter-spacing: -0.32px;
+            """)
+
+        # Sign Up Label
+        signup_label = QLabel("Sign Up with PennyPilot")
+        signup_label.setAlignment(Qt.AlignCenter)
+        signup_label.setStyleSheet("""
+                color: #000;
+                text-align: center;
+                font-family: Inter;
+                font-size: 24px;
+                font-style: normal;
+                font-weight: 600;
+                line-height: 150%; /* 36px */
+                letter-spacing: -0.24px;""")
+
+
+        # Username Input
+        self.username_input = QLineEdit(self)
+        self.username_input.setPlaceholderText("email@domain.com")
+        self.username_input.setFixedSize(250, 40)
+        self.username_input.setStyleSheet("""
+                   QLineEdit {
+                       border: 1px solid #ccc;
+                       border-radius: 8px;
+                       padding: 8px;
+                       font-size: 12pt;
+                       color: black;
+                   }
+               """)
+
+        username_layout = QHBoxLayout()
+        username_layout.addStretch()
+        username_layout.addWidget(self.username_input)
+        username_layout.addStretch()
+
+
+
+        # Password Input
+        self.password_input = QLineEdit(self)
+        self.password_input.setPlaceholderText("Password")
+        self.password_input.setFixedSize(250, 40)
+        self.password_input.setStyleSheet("""
+                          QLineEdit {
+                              border: 1px solid #ccc;
+                              border-radius: 8px;
+                              padding: 8px;
+                              font-size: 12pt;
+                              color: black;
+                          }
+                      """)
+
+        password_layout = QHBoxLayout()
+        password_layout.addStretch()
+        password_layout.addWidget(self.password_input)
+        password_layout.addStretch()
+
+        # Password Input
+        self.retype_password_input = QLineEdit(self)
+        self.retype_password_input.setPlaceholderText("Re-Type Password")
+        self.retype_password_input.setFixedSize(250, 40)
+        self.retype_password_input.setStyleSheet("""
+                                  QLineEdit {
+                                      border: 1px solid #ccc;
+                                      border-radius: 8px;
+                                      padding: 8px;
+                                      font-size: 12pt;
+                                      color: black;
+                                  }
+                              """)
+
+        retype_password_layout = QHBoxLayout()
+        retype_password_layout.addStretch()
+        retype_password_layout.addWidget(self.retype_password_input)
+        retype_password_layout.addStretch()
+
+
+
+        # Create Account Button
+        create_account_button = QPushButton("Create Account")
+        create_account_button.setCursor(QCursor(Qt.PointingHandCursor))
+        create_account_button.setFixedSize(250, 40)
+        create_account_button.setStyleSheet("""
+                            QPushButton {
+                                background-color: black;
+                                color: white;
+                                border: none;
+                                border-radius: 8px;
+                                font-weight: bold;
+                                font-size: 11pt;
+                            }
+                            QPushButton:hover {
+                                background-color: #333;
+                            }
+                        """)
+
+        create_account_layout = QHBoxLayout()
+        create_account_layout.addStretch()
+        create_account_layout.addWidget(create_account_button)
+        create_account_layout.addStretch()
+
+        # Line
+        line = QFrame()
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+        line.setStyleSheet("background-color: #ccc;")
+        line.setFixedHeight(1)
+        line.setFixedWidth(200)  # Fixed width to match your inputs/buttons
+
+        # Center it using a layout
+        line_layout = QHBoxLayout()
+        line_layout.addStretch()
+        line_layout.addWidget(line)
+        line_layout.addStretch()
+
+        # Footer
+        # Terms and Conditions
+        terms_label = QLabel("By clicking continue, you agree to our Terms of Service and Privacy Policy")
+        terms_label.setWordWrap(True)
+        terms_label.setAlignment(Qt.AlignCenter)
+        terms_label.setStyleSheet("""
+            color: gray;
+            font-family: Inter;
+            font-size: 10px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 150%;
+        """)
+        terms_label.setText(
+            'By clicking continue, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.'
+        )
+        terms_label.setOpenExternalLinks(True)
+
+        # Add widgets in correct order
+        main_layout.setSpacing(6)
+        main_layout.addWidget(title)
+        main_layout.addWidget(signup_label)
+        main_layout.addSpacing(10)
+        main_layout.addLayout(username_layout)
+        main_layout.addLayout(password_layout)
+        main_layout.addLayout(retype_password_layout)
+        main_layout.addLayout(create_account_layout)
+
+        main_layout.addLayout(line_layout)
+
+        main_layout.addWidget(
+            terms_label)
+
+        self.setLayout(main_layout)
+
+
+    def login(self):
+        pass
+# Run the app
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = ForgotPasswordWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ View/login_view.py
@@ -0,0 +1,258 @@
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QLabel, QLineEdit,
+    QPushButton, QVBoxLayout, QHBoxLayout, QMessageBox, QFormLayout, QFrame
+)
+from PyQt5.QtGui import QFont, QCursor
+from PyQt5.QtCore import Qt, pyqtSignal
+import sys
+from create_account_view import CreateAccountWindow
+from forgot_password_view import ForgotPasswordWindow
+
+
+#  Custom Clickable QLabel
+class ClickableLabel(QLabel):
+    clicked = pyqtSignal()
+
+    def mousePressEvent(self, event):
+        self.clicked.emit()
+class LoginWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PennyPilot")
+        self.setGeometry(700, 500, 900, 700)
+        self.setStyleSheet("background-color: white;")
+
+
+        main_layout = QVBoxLayout()
+
+        # Title
+        title = QLabel("PennyPilot")
+        title.setAlignment(Qt.AlignCenter)
+        title.setStyleSheet(
+            """
+            color: #000;
+            text-align: center;
+            font-family: Inter;
+            font-size: 32px;
+            font-style: normal; 
+            font-weight: 600;
+            line-height: 150%; /* 48px */
+            letter-spacing: -0.32px;
+            """)
+
+        # Login Label
+        login_label = QLabel("Login")
+        login_label.setAlignment(Qt.AlignCenter)
+        login_label.setStyleSheet("""
+                color: #000;
+                text-align: center;
+                font-family: Inter;
+                font-size: 24px;
+                font-style: normal;
+                font-weight: 600;
+                line-height: 150%; /* 36px */
+                letter-spacing: -0.24px;""")
+
+        # Subtext
+        subtext = QLabel("Enter your email and password to sign in")
+        subtext.setAlignment(Qt.AlignCenter)
+        subtext.setStyleSheet("""
+                color: #000;
+                text-align: center;
+                font-family: Inter;
+                font-size: 16px;
+                font-style: normal;
+                font-weight: 400;
+                margin-top: 0px;
+                line-height: 150%; /* 24px */""")
+
+        # Username Input
+        self.username_input = QLineEdit(self)
+        self.username_input.setPlaceholderText("Username")
+        self.username_input.setFixedSize(250, 40)
+        self.username_input.setStyleSheet("""
+                   QLineEdit {
+                       border: 1px solid #ccc;
+                       border-radius: 8px;
+                       padding: 8px;
+                       font-size: 12pt;
+                       color: black;
+                   }
+               """)
+
+        username_layout = QHBoxLayout()
+        username_layout.addStretch()
+        username_layout.addWidget(self.username_input)
+        username_layout.addStretch()
+
+
+
+        # Password Input
+        self.password_input = QLineEdit(self)
+        self.password_input.setPlaceholderText("Password")
+        self.password_input.setFixedSize(250, 40)
+        self.password_input.setStyleSheet("""
+                          QLineEdit {
+                              border: 1px solid #ccc;
+                              border-radius: 8px;
+                              padding: 8px;
+                              font-size: 12pt;
+                              color: black;
+                          }
+                      """)
+
+        password_layout = QHBoxLayout()
+        password_layout.addStretch()
+        password_layout.addWidget(self.password_input)
+        password_layout.addStretch()
+
+        # Sign in Button
+        sign_in_button = QPushButton("Sign in")
+        sign_in_button.setCursor(QCursor(Qt.PointingHandCursor))
+        sign_in_button.setFixedSize(250, 40)
+        sign_in_button.setStyleSheet("""
+                            QPushButton {
+                                background-color: black;
+                                color: white;
+                                border: none;
+                                border-radius: 8px;
+                                font-weight: bold;
+                                font-size: 11pt;
+                            }
+                            QPushButton:hover {
+                                background-color: #333;
+                            }
+                        """)
+        sign_in_button.clicked.connect(self.login)
+
+        sign_in_layout = QHBoxLayout()
+        sign_in_layout.addStretch()
+        sign_in_layout.addWidget(sign_in_button)
+        sign_in_layout.addStretch()
+
+
+
+        # Forgot my password
+        forgot_my_password_label = ClickableLabel("Forgot my password?")
+        forgot_my_password_label.setAlignment(Qt.AlignCenter)
+        forgot_my_password_label.setStyleSheet("""
+                        color: #5293d9;
+                        text-align: center;
+                        font-family: Inter;
+                        font-size: 16px;
+                        font-style: normal;
+                        font-weight: 400;
+                        line-height: 150%; /* 24px */""")
+        forgot_my_password_label.setCursor(QCursor(Qt.PointingHandCursor))
+        forgot_my_password_label.clicked.connect(self.open_forgot_password_window)
+
+
+        # Maybe a layout
+        forgot_layout = QHBoxLayout()
+        forgot_layout.addStretch()
+        forgot_layout.addWidget(forgot_my_password_label)
+        forgot_layout.addStretch()
+
+        # Line
+        line = QFrame()
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+        line.setStyleSheet("background-color: #ccc;")
+        line.setFixedHeight(1)
+        line.setFixedWidth(200)  # Fixed width to match your inputs/buttons
+
+        # Center it using a layout
+        line_layout = QHBoxLayout()
+        line_layout.addStretch()
+        line_layout.addWidget(line)
+        line_layout.addStretch()
+
+
+
+
+        # Create account button
+        create_button = QPushButton("Create Account")
+        create_button.setCursor(QCursor(Qt.PointingHandCursor))
+        create_button.setFixedSize(250,40)
+        create_button.setStyleSheet("""
+                          QPushButton {
+                              background-color: #f0f0f0;
+                              color: black;
+                              border: none;
+                              border-radius: 8px;
+                              font-weight: bold;
+                              font-size: 11pt;
+                          }
+                          QPushButton:hover {
+                              background-color: #e0e0e0;
+                          }
+                      """)
+        create_button.clicked.connect(self.open_create_account_window)
+
+        create_button_layout = QHBoxLayout()
+        create_button_layout.addStretch()
+        create_button_layout.addWidget(create_button)
+        create_button_layout.addStretch()
+
+        # Footer
+        # Terms and Conditions
+        terms_label = QLabel("By clicking continue, you agree to our Terms of Service and Privacy Policy")
+        terms_label.setWordWrap(True)
+        terms_label.setAlignment(Qt.AlignCenter)
+        terms_label.setStyleSheet("""
+            color: gray;
+            font-family: Inter;
+            font-size: 10px;
+            font-style: normal;
+            font-weight: 400;
+            line-height: 150%;
+        """)
+        terms_label.setText(
+            'By clicking continue, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.'
+        )
+        terms_label.setOpenExternalLinks(True)
+
+        # Add widgets in correct order
+        main_layout.setSpacing(6)
+        main_layout.addWidget(title)
+        main_layout.addWidget(login_label)
+        main_layout.addSpacing(1)
+        main_layout.addWidget(subtext)
+        main_layout.addSpacing(10)
+        main_layout.addLayout(username_layout)
+        main_layout.addLayout(password_layout)
+        main_layout.addLayout(sign_in_layout)
+        main_layout.addWidget(forgot_my_password_label)
+        main_layout.addLayout(forgot_layout)
+        main_layout.addLayout(line_layout)
+        main_layout.addLayout(create_button_layout)
+        main_layout.addWidget(
+            terms_label)
+
+        self.setLayout(main_layout)
+
+
+    def login(self):
+        username = self.lineEdit.text();
+        password = self.lineEdit_2.text();
+        print(password)
+
+    def open_create_account_window(self):
+        self.create_window = CreateAccountWindow()
+        self.create_window.show()
+        self.close()
+
+    def open_forgot_password_window(self):
+        self.forgot_window = ForgotPasswordWindow()
+        self.forgot_window.show()
+
+
+
+
+# Run the app
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = LoginWindow()
+    window.show()
+    sys.exit(app.exec_())
