e05452c9e609a68041abf1b5ed07e328ae5eeab9
Mohammed <ibytemaverick@gmail.com>
2025-05-06 18:24:21 -0700

    Added a separate testing file, and to use for .app or .exc implementations

---
+++ View/main.py
@@ -0,0 +1,13 @@
+# Run the app
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from View.login_view import LoginWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = LoginWindow()
+
+    window.show()
+    sys.exit(app.exec_())
